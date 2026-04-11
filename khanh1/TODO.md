27badbbe4542122ffc3f0f4b7b8650c3591f8e5f
tong <maisolokhanh999@gmail.com>
2026-04-11 13:55:58 +0700

    first commit

---
+++ khanh1/TODO.md
@@ -0,0 +1,1 @@
+4. [x] Fixed Todo.jsx syntax, ready to test&#10;5. [x] Task complete
---
+++ khanh1/src/Page/Home.jsx
@@ -0,0 +1,9 @@
+
+import Todo from '../Components/Todo';
+import './Home.css';
+const Home = () => {
+    return <div >
+        <Todo />
+    </div>
+};
+export default Home;
